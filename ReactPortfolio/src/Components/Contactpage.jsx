a60e28668807c194a50173f5a8fe4fe88f79f091
Suhaim Khalid <suhaimkhalid007@gmail.com>
2024-01-19 22:23:14 +0000

    Update Header Page

---
+++ ReactPortfolio/src/Components/Contactpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contactpage = () => {
+  return (
+    <div>
+      <h1>Welcome to My Contactpage</h1>
+      <p>This is the home page.</p>
+    </div>
+  );
+}
+
+export default Contactpage;
---
+++ ReactPortfolio/src/Components/ProjectPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Project from './Project';
+// import projectsData from '../data/projectsData.json';
+
+const ProjectsPage = () => {
+  return (
+    <div>
+      <h1>My Projects</h1>
+      {/* {projectsData.map((project, index) => (
+        <Project key={index} project={project} />
+      ))} */}
+    </div>
+  );
+}
+
+export default ProjectsPage;
